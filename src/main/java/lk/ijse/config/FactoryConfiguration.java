f844b9d231799ca20c10bc9433c35f054c59685e
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-03-12 13:09:36 +0530

    factory configuration

---
+++ src/main/java/lk/ijse/config/FactoryConfiguration.java
@@ -0,0 +1,29 @@
+package lk.ijse.config;
+
+import lk.ijse.Entity.Book;
+import lk.ijse.Entity.Library;
+import lk.ijse.Entity.Person;
+import lk.ijse.Entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration().configure()
+                .addAnnotatedClass(Book.class).addAnnotatedClass(Person.class).addAnnotatedClass(User.class).addAnnotatedClass(Library.class);
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
