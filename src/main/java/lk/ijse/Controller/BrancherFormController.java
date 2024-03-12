29ba2915baebca22f27f7f7b68e011ad2921b520
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-03-12 11:43:32 +0530

    ui

---
+++ src/main/java/lk/ijse/Controller/BrancherFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class BrancherFormController {
+    public TextField txtLibraryId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtTel;
+    public AnchorPane BranchesPanel;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/lk/ijse/Controller/DashBoardController.java
+++ src/main/java/lk/ijse/Controller/DashBoardController.java
@@ -1,7 +1,29 @@
 package lk.ijse.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class DashBoardController {
     public AnchorPane mainPanel;
+
+    public void btnBookOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/View/Book-Form.fxml"));
+        mainPanel.getChildren().clear();
+        mainPanel.getChildren().add(anchorPane);
+    }
+
+    public void btnPersonOnAtion(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/View/person-Form.fxml"));
+        mainPanel.getChildren().clear();
+        mainPanel.getChildren().add(anchorPane);
+    }
+
+    public void btnBranchersOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/View/Branchers-Form.fxml"));
+        mainPanel.getChildren().clear();
+        mainPanel.getChildren().add(anchorPane);
+    }
 }
