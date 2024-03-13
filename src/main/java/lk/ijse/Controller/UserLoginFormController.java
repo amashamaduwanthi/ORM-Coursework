4a74a29ee20c50485062951244ee41fe0b123c07
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-03-13 19:52:27 +0530

    user registration

--- src/main/java/lk/ijse/Controller/UserLoginFormController.java
+++ src/main/java/lk/ijse/Controller/UserLoginFormController.java
@@ -1,4 +1,24 @@
 package lk.ijse.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class UserLoginFormController {
+    public AnchorPane loginPanel;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSinInOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/View/UserRegistration.fxml"));
+        Scene scene = new Scene(anchorPane);
+        Stage stage = (Stage) loginPanel.getScene().getWindow();
+        stage.setScene(scene);
+
+    }
 }
