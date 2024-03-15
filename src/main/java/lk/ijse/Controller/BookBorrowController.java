d766bc4cd65b30d7cd9e03adc69b68428e8e644d
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-03-15 21:54:31 +0530

    ui

---
+++ src/main/java/lk/ijse/Controller/BookBorrowController.java
@@ -0,0 +1,21 @@
+package lk.ijse.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class BookBorrowController {
+    public Label lblTitle;
+    public Label lblName;
+    public Label lblId;
+    public Label lblGenre;
+    public Label lblAuthor;
+    public Label lblStatus;
+    public TextField txtUserName;
+
+    public void btnBorrowBookOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnConfirmOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/lk/ijse/Controller/UserSearchForm.java
@@ -0,0 +1,11 @@
+package lk.ijse.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class UserSearchForm {
+    public TextField txtTitle;
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+}
